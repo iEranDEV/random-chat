2b5b129bae58e33a37269ac64a49c2e7396d5d47
Narei <ieranofficial@gmail.com>
2022-11-18 23:26:59 +0100

    changed project structure

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,51 @@
 <template>
 	<div>
-		<router-view></router-view>
+		<p>{{ from }}</p>
 	</div>
 </template>
 
 <script>
+import { ref } from 'vue';
 
 export default {
 	setup() {
-		
+		// Setup connection with websocket server
+		const connection = new WebSocket("ws://localhost:3000");
+
+		// Initialize necessary variables
+		const messages = ref([]);
+		const from = ref(null);
+		const to = ref(null);
+
+		// On message event listener
+		connection.onmessage = (event) => {
+			let data = JSON.parse(event.data);
+			switch(data.type) {
+				// Connection setup return message from server with ID
+				case 'CONNECTION_SETUP':
+					from.value = data.from;
+					break;
+
+				// Server assigned user to your chat
+				case 'USER_RESPONSE':
+					break;
+				
+				// User disconnected from chat (chat ended)
+				case 'USER_DISCONNECTED':
+					break;
+
+				// Message event
+				case 'MESSAGE':
+					break;
+			}
+		}
+
+		return {
+			connection,
+			messages,
+			from,
+			to
+		}
 	},
 }
 </script>
--- server/index.js
+++ server/index.js
@@ -6,24 +6,36 @@ const server = new WebSocket.Server({
 
 // List of all connected user
 const clients = new Map();
-let next = null;
+// let next = null;
 
 // On connection to server event
 server.on('connection', (connection) => {
     const id = uuidv4();
     clients.set(id, connection);
+
+    // Send initial connection setup message to user with ID
     connection.send(JSON.stringify({
-        type: 'CONNECTION',
-        id: id,
+        type: 'CONNECTION_SETUP',
+        from: id,
     }))
 
-    //connection.send("ID = " + id);
 
-    connection.on('message', (messageAsString) => {
-        const message = JSON.parse(messageAsString);
-        message.from = id;
+    // On message from user event (user => server)
+    connection.on('message', (message) => {
+        const data = JSON.parse(message);
+        data.from = id;
 
-        if(message.type === "MESSAGE") {
+        switch(data.type) {
+            // Request from user to create a chat
+            case 'USER_REQUEST':
+                break;
+
+            // Send message to another user
+            case 'MESSAGE':
+                break;
+        }
+        
+        /*if(message.type === "MESSAGE") {
             // Sending message to receiver
             if(message.to != null) {
                 clients.get(message.to).send(JSON.stringify({
@@ -46,19 +58,20 @@ server.on('connection', (connection) => {
             } else {
                 next = id;
             }
-        }
+        }*/
 
-    })
+    });
 
     connection.on('close', () => {
         clients.delete(connection);
-        if(next == id) next = null;
+        //if(next == id) next = null;
     })
 })
 
+// Generating UUID for connection
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
     });
-  }
+}
